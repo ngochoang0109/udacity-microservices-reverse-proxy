003f0237756bfb15fcd657e13695d598218dcb26
hoangtn <hoangtn0109@gmail.com>
2024-09-29 05:44:02 +0700

    fix bug

--- deployDocker.sh
+++ deployDocker.sh
@@ -1,4 +1,4 @@
-docker build -t ngochoang010900/reverseproxy .
+docker build -t ngochoang010900/reverseproxy:latest .
 docker tag reverseproxy ngochoang010900/reverseproxy
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push ngochoang010900/reverseproxy
